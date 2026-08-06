2f2c5b0dee30ef21426d8547b469498fb1995283
tushar <tushar.mehta466@gmail.com>
2026-08-06 10:16:41 +0530

    No cache added

--- app/src/main/java/com/example/sduiassignment/data/remote/ApiService.kt
+++ app/src/main/java/com/example/sduiassignment/data/remote/ApiService.kt
@@ -2,8 +2,10 @@ package com.example.sduiassignment.data.remote
 
 import com.example.sduiassignment.data.model.DiscoverPageResponse
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface ApiService {
+    @Headers("Cache-Control: no-cache")
     @GET("a9f60712115da1ebeb93")
     suspend fun getHomePage(): DiscoverPageResponse
 }
